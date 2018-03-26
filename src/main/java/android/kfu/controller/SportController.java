f1ba00735ce6dcdc35bb416daef0f5b985a3bb21
Nurislam <snurislam97@mail.ru>
2018-03-26 18:11:20 +0300

    //

--- src/main/java/android/kfu/controller/SportController.java
+++ src/main/java/android/kfu/controller/SportController.java
@@ -72,7 +72,7 @@ public class SportController {
     @RequestMapping(method = RequestMethod.GET)
     public ApiResult allSports() {
         ApiResult result = new ApiResult(errorCodes.getSuccess());
-        addKoS("Volleyball");
+//        addKoS("Volleyball");
         try{
             result.setBody(service.getAll());
         } catch (KindOfSportNotFoundException ex){
